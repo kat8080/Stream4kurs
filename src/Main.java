87795aeb65b62070f45f6a49311694c5be3bd93c
Katya <K_at@mail.ru>
2023-02-01 19:46:00 +0800

    Stream API и optional

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        findMinMax(Stream.of(1, 3, 4, 5, 8, 56, -54),
+                Integer::compareTo, (min, max) -> {
+                    System.out.println("min: " + min + "   max: " + max);
+                });
+
+        List<Integer> number = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).toList();
+        byEvenNumber(number.stream());
+    }
+
+    //ЗАДАЧА 1
+    public static <T> void findMinMax(Stream<? extends T> stream,
+                                          Comparator<? super T> order,
+                                          BiConsumer<? super T, ? super T> minMaxConsumer) {
+            List<T> list = stream.sorted(order).collect(Collectors.toList());
+            minMaxConsumer.accept(list.get(0), list.get(list.size() - 1));
+        }
+
+    //ЗАДАЧА 2
+    public static void byEvenNumber(Stream<Integer> number) {
+        List<Integer> l = number.filter(i -> i % 2 == 0).toList();
+        System.out.println("Колличество четных чисел " + l.size());
+        l.forEach(i -> System.out.print(i+" "));
     }
 }
